cb814bc092fb931ba163211ee7ced1b87d827144
arshita <arshitapatel077@gmail.com>
2025-02-11 10:21:36 +0530

    Add files via upload

---
+++ prostackclass5.js
@@ -0,0 +1,7 @@
+//write a program 3 digit or not
+let num1=4000
+if(num1>=100 && num1<=999 ){
+    console.log("yes given number is 3 digit number")
+}else{
+    console.log("not a three digit number")
+}
