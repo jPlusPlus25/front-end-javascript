14ee7c71190ee72786423b4b620f41c8706cf7a1
arshita <arshitapatel077@gmail.com>
2025-02-12 10:20:44 +0530

    Add files via upload

---
+++ arrayerror1.js
@@ -0,0 +1,11 @@
+let eid=[101,102,103,104]
+//index  0   1   2   3
+console.log(eid)
+//how to read array element -using indexing
+console.log(eid[0])//101
+console.log(eid[1])//102
+console.log(eid[2])//103
+console.log(eid[3])//104
+console.log(eid[4])//undefined
+
+//i show error ->undefined bsc 4 index is not
