7ac4d42c0a93f389064cdd6254133deb98f52197
arshita <arshitapatel077@gmail.com>
2025-02-12 10:17:14 +0530

    Add files via upload

---
+++ error2.js
@@ -0,0 +1,8 @@
+let a=[]
+let b=[10,20,30,40]
+let c=[10,20,5,"rahul","rahul",undefined,null,true,[],{}]
+//allowed duplicates and heterogeneous element
+//read -Array
+console.log(a);
+console.log(b);
+console.log(c);
