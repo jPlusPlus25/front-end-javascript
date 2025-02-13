6683e9ed437ce8c5e21d24a3b88a98d8e40c3133
arshita <arshitapatel077@gmail.com>
2025-02-13 11:37:24 +0530

    Add files via upload

---
+++ object1.js
@@ -0,0 +1,15 @@
+let a={}//empty object
+let b={id:101}
+let emp={eid:101,
+    ename:'rahul',
+    esal:45000
+}
+//how to read
+console.log(a)
+console.log(b)
+console.log(emp)
+console.log(emp.eid)
+console.log(emp.ename)
+console.log(emp.esal)
+console.log(emp.eage)//undefiened ->reading object values using key/property name,if key not avalable we get undefied
+
