ff49496c8b6360566c1fa29458945f4ed6150ae9
arshita <arshitapatel077@gmail.com>
2025-02-13 11:37:51 +0530

    Add files via upload

---
+++ object2.js
@@ -0,0 +1,10 @@
+let emp={eid:101,
+    ename:'Arshita',
+    esal:48000.5}
+ //upadate and delete
+emp.esal=555000
+emp.emailid='arshit07@gmail.com'
+console.log(emp)
+//how to delet
+delete emp.esal
+console.log(emp)
