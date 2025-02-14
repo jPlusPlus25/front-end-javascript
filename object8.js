1f5cd51b6f57612a85f2687692097594f9db616a
arshita <arshitapatel077@gmail.com>
2025-02-14 10:02:17 +0530

    //by written key ,it will give u key names only
    //by written user[key],it will give u  only values

---
+++ object8.js
@@ -0,0 +1,7 @@
+let user={
+    uid:101,uname:"Arshita",loc:"Gujrat",email:"arshitapatel077@gmail.com",age:25,stause:"not single"}
+
+for(let key in user){
+    console.log(key)//by written key ,it will give u key names only
+    console.log(user[key])//by written user[key],it will give u  only values
+}
