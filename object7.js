57535a186d896a6125acee6bf846bc386ddf1bf0
arshita <arshitapatel077@gmail.com>
2025-02-14 10:11:54 +0530

    object duplicate are not allowed

---
+++ object7.js
@@ -0,0 +1,4 @@
+let ids=[101,102,104,105,102,103,104,101]
+let users={uid:101,uname:"arshita",uid:102,uid:104}
+console.log(ids)//object order is maintain and duplicate are allowed
+console.log(users)//object duplicate are not allowed
